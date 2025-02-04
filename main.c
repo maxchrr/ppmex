9036b5c963bb43d837bcd4283fd7530e076b6d23
Max Charrier <max@puffer.fish>
2025-02-04 17:06:31 +0100

    fix mistake
    Signed-off-by: Max Charrier <max@puffer.fish>

--- main.c
+++ main.c
@@ -8,8 +8,8 @@ int main(int argc, char* argv[]) {
 	int width, height;
 
 	if (argc == 3) {
-		width = strtol(argv[1], NULL, 10);;
-		height = strtol(argv[2], NULL, 10);;
+		width = strtol(argv[1], NULL, 10);
+		height = strtol(argv[2], NULL, 10);
 	}
 	else {
 		width = 8;
